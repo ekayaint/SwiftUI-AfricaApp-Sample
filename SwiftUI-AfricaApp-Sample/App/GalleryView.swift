03c595aeaaa809ef5e49f8a0306cb16abf892228
ekayaint <ekayaint@gmail.com>
2023-07-18 15:03:16 +0300

    First launch is ok

---
+++ SwiftUI-AfricaApp-Sample/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  SwiftUI-AfricaApp-Sample
+//
+//  Created by ekayaint on 18.07.2023.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
---
+++ SwiftUI-AfricaApp-Sample/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  SwiftUI-AfricaApp-Sample
+//
+//  Created by ekayaint on 18.07.2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
