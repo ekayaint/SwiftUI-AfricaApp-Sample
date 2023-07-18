d135a29b94cab5b47d618739db80190663e40d85
ekayaint <ekayaint@gmail.com>
2023-07-18 13:00:32 +0300

    Initial Commit

---
+++ SwiftUI-AfricaApp-Sample/SwiftUI_AfricaApp_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AfricaApp_SampleApp.swift
+//  SwiftUI-AfricaApp-Sample
+//
+//  Created by ekayaint on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AfricaApp_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
