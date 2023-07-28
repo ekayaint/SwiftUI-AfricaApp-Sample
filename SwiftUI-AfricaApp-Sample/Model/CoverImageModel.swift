7607bedcd55b180da5d14677c6947ff052c99fbc
ekayaint <ekayaint@gmail.com>
2023-07-28 19:53:46 +0300

    Cover Image ok

---
+++ SwiftUI-AfricaApp-Sample/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  SwiftUI-AfricaApp-Sample
+//
+//  Created by ekayaint on 28.07.2023.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name : String
+}
