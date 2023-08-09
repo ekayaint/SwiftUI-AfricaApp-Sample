f86a0160199a178f0aa4b469084c06edc8cac1ea
ekayaint <ekayaint@gmail.com>
2023-08-09 14:00:43 +0300

    Video part is ok

--- SwiftUI-AfricaApp-Sample/Helper/VideoPlayerHelper.swift
+++ SwiftUI-AfricaApp-Sample/Helper/VideoPlayerHelper.swift
@@ -9,3 +9,11 @@ import Foundation
 import AVKit
 
 var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
--- SwiftUI-AfricaApp-Sample/View/VideoPlayerView.swift
+++ SwiftUI-AfricaApp-Sample/View/VideoPlayerView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 import AVKit
 
 struct VideoPlayerView: View {
+    var videoSelected: String
+    var videoTitle: String
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "cheetah", withExtension: "mp4")!))
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")){
+                //Text(videoTitle)
+            }
+            .overlay(
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .padding(.top, 6)
+                .padding(.horizontal, 8)
+            
+            ,alignment: .topLeading
+            )
+            
+        } //: VStack
+        .accentColor(.accentColor)
+        .navigationTitle(videoTitle)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct VideoPlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        VideoPlayerView()
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "lion")
+        }
     }
 }
