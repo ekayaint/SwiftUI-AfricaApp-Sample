786e5d601faa170e94df054beadd35cab7ff928f
ekayaint <ekayaint@gmail.com>
2023-08-11 13:59:36 +0300

    Gallery is ok

--- SwiftUI-AfricaApp-Sample/App/GalleryView.swift
+++ SwiftUI-AfricaApp-Sample/App/GalleryView.swift
@@ -8,20 +8,58 @@
 import SwiftUI
 
 struct GalleryView: View {
-    let gridLayout: [GridItem] = [
+    @State private var selectedAnimal: String = "lion"
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    /*let gridLayout: [GridItem] = [
         GridItem(.flexible()),
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    */
+    
+    //let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            // MARK: - Grid
-            
-            LazyVGrid(columns: gridLayout,alignment: .center, spacing: 10) {
-                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                    Text("Gallery")
-                }
-            }
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - Image
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // MARK: - Slider
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: {value in
+                        gridSwitch()
+                    })
+                // MARK: - Grid
+                LazyVGrid(columns: gridLayout,alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    } //: ForEach
+                } //: Grid
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } // : VStack
+          .padding(.horizontal, 10)
+          .padding(.vertical, 50)
         } //: ScrollView
         .frame(maxWidth: .infinity, maxHeight: .infinity)
        // .background(MotionAnimationView())
